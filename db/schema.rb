8b93569d180ca9107941b582293b2470be9905a9
amanuelgt <aababiya@msudenver.edu>
2024-09-12 13:51:41 -0600

    test destroy scaffold and db

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_12_173800) do
-  create_table "students", force: :cascade do |t|
-    t.string "name"
-    t.string "school_email"
-    t.string "major"
-    t.string "minor"
-    t.date "graduation_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 0) do
 end
