450aad13d9be561debe3987c11df60845de22f99
amanuelgt <aababiya@msudenver.edu>
2024-09-24 17:39:21 -0600

    Updated code for deafult image and more testing

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,8 +4,9 @@ class Student < ApplicationRecord
 
     validates :first_name, presence:true
     validates :last_name, presence:true
-    validates :school_email, presence:true, uniqueness:true
+    validates :school_email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+(?:\.[\w+\-.]+)*@msudenver\.edu\z/i, message: 'must be a valid email address' }
     validates :major, presence:true
-    # validates :expected_graduation_date, presence true
+    validates :expected_graduation_date, presence: true
+
 
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -7,10 +7,40 @@ class StudentTest < ActiveSupport::TestCase
 
   test"should raise error when saving student without first name" do
     assert_raises ActiveRecord::RecordInvalid do
-      Student.create!(first_name:"Ali", last_name:"Bira", school_email:"abira@msudenver.edu", major:"CS")
+      Student.create!(first_name:"Ali")
+    end
+  end
+
+  test"should raise error when saving student without last name" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(last_name:"Bira")
+    end
+  end
+
+  test"should raise error when saving student without major" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(major:"CS")
     end
   end
 
 
+  test 'should raise error when saving school email with invalid format' do
+    # Invalid emails list
+    invalid_emails = ['user','user@','user@com','@gmail.com','name@msudenver.com', 'name.doe@msudenver.edu.edu']
+
+    invalid_emails.each do |email|
+      student = Student.new(school_email: email)
+      assert_not student.valid?
+      assert_includes student.errors[:school_email], 'must be a valid email address'
+    end
+  end
+
+
+  test "should raise error when saving student without expected date" do
+    assert_raises ActiveRecord::RecordInvalid do
+    # expected date should be a year from today
+      Student.create!(expected_graduation_date: Date.today + 1.year)
+    end
+  end
 
 end
