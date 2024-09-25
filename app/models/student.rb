9316f64fdd28597d274f899c7a80c3199beea3c3
ababiya24 <ababiyaamanuel@gmail.com>
2024-09-24 21:14:35 -0600

    final code for iteration 01

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,6 +5,7 @@ class Student < ApplicationRecord
 
     validates :first_name, presence:true
     validates :last_name, presence:true
+    # added regular expression to check email follows format
     validates :school_email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+(?:\.[\w+\-.]+)*@msudenver\.edu\z/i, message: 'must be a valid email address' }
     validates :major, presence:true
     validates :expected_graduation_date, presence: true
