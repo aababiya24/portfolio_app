34713a5b168804ec8cfdb1eaaca945880f925e5b
ababiya24 <ababiyaamanuel@gmail.com>
2024-10-08 11:35:20 -0600

    added dropdown menu for major selection and also cleaned up the database

--- app/models/student.rb
+++ app/models/student.rb
@@ -11,4 +11,11 @@ class Student < ApplicationRecord
     validates :expected_graduation_date, presence: true
 
 
+    VALID_MAJORS =  ["Computer Engineering BS", "Computer Information Systems BS",
+    "Computer Science BS", "Cybersecurity Major", "Data Science and Machine Learning Major"]
+
+    validates :major, inclusion: { in: VALID_MAJORS, message: "%{value} is not a valid major" }
+
+
+
 end
